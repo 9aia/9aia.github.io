b68fc5fb2e37b150b2b92dd20d25096029c7192f
Luis Float <contact@luisfloat.com>
2022-07-20 17:29:59 -0300

    feat(main): init

---
+++ src/main/.vitepress/config.js
@@ -0,0 +1,19 @@
+export default {
+    title: 'Inforg',
+    description: 'Inforg landpage website',
+    appearance: true,
+    themeConfig: {
+        siteTitle: 'Inforg',
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/inforgdev' },
+            { icon: 'twitter', link: 'https://twitter.com/inforgdev' },
+            { icon: 'instagram', link: 'https://instagram.com/inforgdev' },
+        ],
+        nav: [
+            { text: 'SpongiaCSS', link: 'https://inforg.dev/spongia-css' },
+        ],
+        footer: {
+            copyright: '2022 Inforg',
+        },
+    }
+}
---
+++ src/main/index.md
@@ -0,0 +1,11 @@
+---
+layout: home
+
+hero:
+  name: Inforg
+  text: Dev tools experiment lab.
+  actions:
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/inforgdev
+---
