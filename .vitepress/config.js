3b7b69986b880908fbd7f5934ff891f232dc7602
Luis Float <contact@luisfloat.com>
2022-10-20 11:26:00 -0300

    refactor(main): move views into `views/`

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -3,7 +3,7 @@ import themeConfig from "../src/main/themeConfig.js";
 export default {
     title: 'Inforg',
     description: 'Inforg landpage website',
-    srcDir: 'src/main',
+    srcDir: 'src/main/views/',
     outDir: 'dist/',
     themeConfig,
 }
