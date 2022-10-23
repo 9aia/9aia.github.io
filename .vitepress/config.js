2632e3dad2428b8a442cb71fbb8c6048e61606cc
Luis Float <contact@luisfloat.com>
2022-10-22 22:29:23 -0300

    style(build): use double quote marks instead

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,9 +1,9 @@
 import themeConfig from "../src/main/themeConfig.js";
 
 export default {
-    title: 'Inforg',
-    description: 'Inforg landpage website',
-    srcDir: 'src/main/views/',
-    outDir: 'dist/',
+    title: "Inforg",
+    description: "Inforg landpage website",
+    srcDir: "src/main/views/",
+    outDir: "dist/",
     themeConfig,
 };
